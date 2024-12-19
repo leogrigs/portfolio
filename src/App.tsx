993b67f944b9062d49ebd5a50ed9161f03907597
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-19 09:19:14 -0300

    style: refactor App component layout and enhance styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hi, my name is</h1>
-      <h2>Leonardo Grigorio Ferreira</h2>
-      <h3>I'm a Frontend Developer</h3>
-      <p>
+    <main className="h-screen bg-slate-950 flex flex-col justify-center items-center text-center gap-4 px-4">
+      <h1 className="text-sm text-sky-300 mb-2">Hi, my name is</h1>
+      <h2 className="text-4xl font-bold text-white mb-2">
+        Leonardo Grigorio Ferreira
+      </h2>
+      <h3 className="text-xl text-sky-400 mb-4">I'm a Frontend Developer</h3>
+      <p className="text-md text-slate-300 max-w-md">
         I'm a Frontend Developer with a passion for building beautiful and
-        functional user interfaces.
+        functional user interfaces. I enjoy crafting modern web experiences with
+        clean, scalable, and user-friendly designs.
       </p>
-    </>
+    </main>
   );
 }
 
