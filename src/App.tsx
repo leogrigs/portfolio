faa1c72f5e31aec065f9be4dfafeb6e873615df8
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-19 09:08:07 -0300

    chore: initialize React + TypeScript + Vite project with basic configuration and structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hi, my name is</h1>
+      <h2>Leonardo Grigorio Ferreira</h2>
+      <h3>I'm a Frontend Developer</h3>
+      <p>
+        I'm a Frontend Developer with a passion for building beautiful and
+        functional user interfaces.
+      </p>
+    </>
+  );
+}
+
+export default App;
