742fe264be97c12975dcbe79ab6a34e197b22fed
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-21 12:44:05 -0300

    feat: create Summary component and update App layout with new structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css";
+import Summary from "./sections/Summary";
 
 function App() {
   return (
-    <main className="h-screen bg-slate-950 flex flex-col justify-center items-center text-center gap-4 px-4">
-      <h1 className="text-sm text-sky-300 mb-2">Hi, my name is</h1>
-      <h2 className="text-4xl font-bold text-white mb-2">
-        Leonardo Grigorio Ferreira
-      </h2>
-      <h3 className="text-xl text-sky-400 mb-4">I'm a Frontend Developer</h3>
-      <p className="text-md text-slate-300 max-w-md">
-        I'm a Frontend Developer with a passion for building beautiful and
-        functional user interfaces. I enjoy crafting modern web experiences with
-        clean, scalable, and user-friendly designs.
-      </p>
+    <main>
+      <Summary />
     </main>
   );
 }
