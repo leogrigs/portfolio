e522fd0b60c2ff8385b4686fe8dcb7fcd96a4ca6
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-22 18:33:42 -0300

    feat: add navigation menu component and integrate it into the App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { AppNavigationMenu } from "./components/AppNavigationMenu";
 import Summary from "./sections/Summary";
 
 function App() {
   return (
     <main>
+      <AppNavigationMenu />
       <Summary />
     </main>
   );
---
+++ src/components/AppNavigationMenu.tsx
@@ -0,0 +1,34 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+const links = [
+  { href: "#summary", text: "Summary" },
+  { href: "#experience", text: "Experience" },
+  { href: "#projects", text: "Projects" },
+  { href: "#contact", text: "Contact" },
+];
+
+export function AppNavigationMenu() {
+  return (
+    <header className="fixed top-0 z-10 flex justify-center items-center w-full bg-slate-900">
+      <NavigationMenu>
+        <NavigationMenuList className="space-x-6 px-8 py-4">
+          {links.map((link) => (
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                href={link.href}
+                className="text-slate-300 hover:text-sky-400 hover:border-b border-sky-400 transition-colors duration-300 text-sm md:text-base"
+              >
+                {link.text}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+}
