388abd4c3b07f6f29d9e844f78bfb3a0bda1e52b
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-24 14:38:56 -0300

    feat: update About section for improved clarity and layout; enhance text content and add badge link

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -7,59 +7,42 @@ export default function About() {
       nextSectionId="experience"
       sectionTitle="About"
     >
-      <div className="flex flex-col lg:flex-row justify-center items-center gap-8 lg:gap-16 px-6 md:px-16">
-        <div className="lg:max-w-full lg:w-1/2">
-          <p className="text-md md:text-lg text-slate-300 mb-6">
-            Hello! My name is Leonardo, and I enjoy creating web interfaces that
-            combine design and functionality. My journey in web development
-            began with a deep curiosity for building digital solutions and a
-            knack for problem-solving. Over time, I honed my skills and
-            specialized in Angular, focusing on delivering accessible, engaging,
-            and high-performing web applications.
+      <div className="flex flex-col lg:flex-row justify-between items-start gap-8">
+        {/* Text Section */}
+        <div className="lg:w-1/2 space-y-4 mb-8">
+          <p className="text-md md:text-lg text-slate-400">
+            Hi, I’m Leonardo, a Front-End Developer focused on building
+            accessible and high-performing web applications with expertise in
+            Angular, SPAs, and microfrontends.
           </p>
-          <p className="text-md md:text-lg text-slate-300 mb-6">
-            Currently, I am a Software Engineer at NTT DATA, one of the largest
-            technology consultancies in the world. Here, I contribute to
-            projects for Itaú Private Bank, a division of Itaú Unibanco,
-            offering personalized and exclusive financial services to
-            high-income clients. In this role, I work on modernizing web
-            platforms to enhance user experience, accessibility, and
-            scalability, achieving impactful results such as a 400% increase in
-            engagement rates.
+          <p className="text-md md:text-lg text-slate-400">
+            At NTT DATA, I’ve contributed to modernizing web platforms for Itaú
+            Private Bank, enhancing user experience and accessibility, and
+            achieving impactful results like a 400% engagement increase.
           </p>
-          <p className="text-md md:text-lg text-slate-300 mb-6">
-            I’m also experienced in building SPAs and microfrontends with
-            Angular Elements, using reactive programming with RxJS, state
-            management with NgRx, and tools like Nx for monorepos. My backend
-            work involves creating BFFs in Node.js, prioritizing accessibility
-            and functionality with agile frameworks such as Scrum.
+          <p className="text-md md:text-lg text-slate-400">
+            I bring skills in Angular Elements, RxJS, NgRx, and Node.js, along
+            with a strong foundation in agile methodologies and accessibility
+            best practices.
           </p>
-          <p className="text-md md:text-lg text-slate-300 mb-6">
-            I hold a degree in Information Systems from the University of São
-            Paulo (USP) and have advanced proficiency in English. I'm constantly
-            seeking meaningful challenges where I can contribute my expertise
-            while promoting collaborative and motivating work environments.
-          </p>
-          <h3 className="text-xl font-semibold text-sky-400 mt-8">
-            Technologies I’ve Been Working With:
-          </h3>
-          <ul className="list-disc pl-6 mt-4 space-y-2 text-slate-300">
-            <li>JavaScript (ES6+)</li>
-            <li>TypeScript</li>
-            <li>Angular</li>
-            <li>React</li>
-            <li>Node.js</li>
-            <li>RxJS</li>
-            <li>NgRx</li>
-            <li>GA4</li>
-          </ul>
         </div>
-        <div className="w-56 md:w-72">
+
+        {/* Image Section */}
+        <div className="w-40 md:w-56 lg:w-80 relative">
           <img
-            className="rounded-full w-full h-auto object-cover border-2 border-slate-100"
+            className="rounded-full w-full h-auto object-cover border border-slate-100"
             src="https://media.licdn.com/dms/image/v2/D4D03AQFwyDAhI0EPEA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1728589234021?e=1740614400&v=beta&t=y6wvt45X0d2oswFED4eAGuxFJQPcVRTcWZdN7toii1c"
             alt="Leonardo Grigorio Ferreira"
           />
+          <a
+            className="absolute bottom-0 right-8"
+            href="https://www.credly.com/badges/08549e01-a715-4af4-9188-2f7cb18cf207"
+          >
+            <img
+              src="https://miro.medium.com/v2/resize:fit:512/0*P7dmdm5OCZaMEPOG"
+              width="64"
+            />
+          </a>
         </div>
       </div>
     </AppSectionWrapper>
