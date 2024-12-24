3449712079694edb7a81bd1169abf2dd2ec63f44
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-24 12:42:25 -0300

    feat: add AppSectionWrapper component for structured section layout and navigation

---
+++ src/components/AppSectionWrapper.tsx
@@ -0,0 +1,25 @@
+import AppNextSectionButton from "./AppNextSectionButton";
+
+interface AppSectionWrapperProps {
+  children: React.ReactNode;
+  sectionId: string;
+  nextSectionId?: string;
+}
+
+export default function AppSectionWrapper({
+  children,
+  sectionId,
+  nextSectionId,
+}: AppSectionWrapperProps) {
+  return (
+    <section
+      id={sectionId}
+      className="min-h-screen flex flex-col justify-center transition-colors duration-300"
+    >
+      {children}
+      <div className="flex justify-center mt-auto mb-24">
+        <AppNextSectionButton sectionId={nextSectionId ?? "summary"} />
+      </div>
+    </section>
+  );
+}
