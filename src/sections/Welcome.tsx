d40a525bff397acdc7761b1750f669692a5b40ae
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-25 23:09:43 -0300

    style: update gradient colors in Welcome section for consistency with design variables

--- src/sections/Welcome.tsx
+++ src/sections/Welcome.tsx
@@ -8,16 +8,16 @@ export default function Welcome() {
         <h1 className="text-sm md:text-md bg-gradient-to-r from-sky-400 to-slate-50 bg-clip-text text-transparent mb-2">
           Hi, my name is
         </h1>
-        <h2 className="text-3xl md:text-7xl font-extrabold bg-gradient-to-r from-slate-300 to-sky-400 bg-clip-text text-transparent leading-relaxed">
+        <h2 className="text-3xl md:text-7xl font-extrabold bg-gradient-to-r from-foreground to-sky-400 bg-clip-text text-transparent leading-relaxed">
           Leonardo
         </h2>
-        <h2 className="text-3xl md:text-7xl font-extrabold bg-gradient-to-r from-slate-300 to-sky-400 bg-clip-text text-transparent mb-6 !leading-tight">
+        <h2 className="text-3xl md:text-7xl font-extrabold bg-gradient-to-r from-foreground to-sky-400 bg-clip-text text-transparent mb-6 !leading-tight">
           Grigorio Ferreira.
         </h2>
         <h3 className="text-2xl md:text-3xl font-semibold bg-gradient-to-r from-sky-400 to-slate-50 bg-clip-text text-transparent mb-12">
           Front-End Developer
         </h3>
-        <p className="text-md md:text-lg font-light text-slate-400 leading-relaxed mb-6">
+        <p className="text-md md:text-lg font-light text-muted-foreground leading-relaxed mb-6">
           I’m a dedicated Front-End Developer with over three years of
           experience, specializing in Angular. I create intuitive, accessible,
           and user-friendly web interfaces for diverse audiences while expanding
