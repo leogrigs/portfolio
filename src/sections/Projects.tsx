47c5198ca51f24a695b36b530908d38d0695856b
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-27 18:59:26 -0300

    feat: update Projects section to use AppCard component for dynamic project display

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,4 +1,6 @@
+import AppCard from "@/components/AppCard";
 import AppSectionWrapper from "@/components/AppSectionWrapper";
+import { PROJECTS } from "@/content/projects.const";
 
 export default function Projects() {
   return (
@@ -7,15 +9,9 @@ export default function Projects() {
         Here are some of the projects I've worked on. Click on a project to
         learn more.
       </h3>
-      <div className="flex justify-between items-center">
-        {[1, 2, 3].map((project) => (
-          <div
-            key={project}
-            className="border border-slate-600 rounded-md p-8 min-h-72"
-          >
-            <h3>Project {project}</h3>
-            <p>Project description</p>
-          </div>
+      <div className="flex justify-between gap-8 items-center">
+        {PROJECTS.map((project) => (
+          <AppCard card={project} />
         ))}
       </div>
     </AppSectionWrapper>
