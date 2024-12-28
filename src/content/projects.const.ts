b55dbfde4c64c1d7ed8929b16636f5616f169d72
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-12-28 18:37:56 -0300

    feat: update project descriptions and technologies in PROJECTS constant for better clarity and detail

--- src/content/projects.const.ts
+++ src/content/projects.const.ts
@@ -2,21 +2,41 @@ import { ICard } from "@/interfaces/card.interface";
 
 export const PROJECTS: ICard[] = [
   {
-    cardTitle: "Project 1",
-    cardSubtitle: "Subtitle 1",
-    highlights: ["Highlight 1", "Highlight 2", "Highlight 3"],
-    technologies: ["Tech 1", "Tech 2", "Tech 3"],
+    cardTitle: "Easy Cards",
+    cardSubtitle: "A Free Flashcard Application for Smarter Studying",
+    highlights: [
+      "Enabled efficient learning with customizable flashcards and interactive features.",
+      "Built with an intuitive interface to enhance user engagement and learning experience.",
+      "Completely free and accessible, empowering students and professionals to master material effortlessly.",
+    ],
+    technologies: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Firebase",
+      "Tailwind CSS",
+      "shadcn/ui",
+      "Jest",
+    ],
   },
   {
-    cardTitle: "Project 2",
-    cardSubtitle: "Subtitle 2",
-    highlights: ["Highlight 1", "Highlight 2", "Highlight 3"],
-    technologies: ["Tech 1", "Tech 2", "Tech 3"],
+    cardTitle: "Easy Budget",
+    cardSubtitle: "Simplifying Personal Finance Management",
+    highlights: [
+      "Streamlined expense tracking and budgeting with an intuitive interface.",
+      "Developed a visually engaging platform to provide insights into spending habits.",
+      "Free, efficient, and accessible, designed to fill a gap in personal finance tools.",
+    ],
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite", "Firebase"],
   },
   {
-    cardTitle: "Project 3",
-    cardSubtitle: "Subtitle 3",
-    highlights: ["Highlight 1", "Highlight 2", "Highlight 3"],
-    technologies: ["Tech 1", "Tech 2", "Tech 3"],
+    cardTitle: "Easy Focus",
+    cardSubtitle: "Pomodoro Tracker for Focused Studying",
+    highlights: [
+      "Boosted study productivity with a simple and effective Pomodoro tracker.",
+      "Integrated relaxing music to create a focused learning environment.",
+      "User-friendly design for seamless use in study sessions.",
+    ],
+    technologies: ["HTML", "CSS", "JavaScript", "React"],
   },
 ];
