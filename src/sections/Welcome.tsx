e29565bfba8b6eea8c65aaa15143752d0cdbdb9d
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2025-01-01 23:15:42 -0300

    style: update Welcome section to use fontColor variable for improved theming

--- src/sections/Welcome.tsx
+++ src/sections/Welcome.tsx
@@ -5,16 +5,16 @@ export default function Welcome() {
     <AppSectionWrapper sectionId="welcome" nextSectionId="about">
       <div className="flex flex-col justify-center">
         {/* Text Content */}
-        <h1 className="text-sm md:text-md bg-gradient-to-r from-sky-400 to-slate-50 bg-clip-text text-transparent mb-2">
+        <h1 className="text-sm md:text-md bg-gradient-to-r from-fontColor to-foreground bg-clip-text text-transparent mb-2">
           Hi, my name is
         </h1>
-        <h2 className="text-3xl md:text-7xl font-bold bg-gradient-to-r from-foreground to-sky-400 bg-clip-text text-transparent leading-relaxed">
+        <h2 className="text-3xl md:text-7xl font-bold bg-gradient-to-r from-foreground to-fontColor bg-clip-text text-transparent leading-relaxed">
           Leonardo
         </h2>
-        <h2 className="text-3xl md:text-7xl font-bold bg-gradient-to-r from-foreground to-sky-400 bg-clip-text text-transparent mb-6 !leading-tight">
+        <h2 className="text-3xl md:text-7xl font-bold bg-gradient-to-r from-foreground to-fontColor bg-clip-text text-transparent mb-6 !leading-tight">
           Grigorio Ferreira.
         </h2>
-        <h3 className="text-2xl md:text-3xl font-semibold bg-gradient-to-r from-sky-400 to-slate-50 bg-clip-text text-transparent mb-12">
+        <h3 className="text-2xl md:text-3xl font-semibold bg-gradient-to-r from-fontColor to-foreground bg-clip-text text-transparent mb-12">
           Front-End Developer
         </h3>
         <p className="text-md md:text-lg font-light text-muted-foreground leading-relaxed mb-6">
