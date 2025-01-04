ba6c0fd418a6ceeb5000817fb26f53e99700695d
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2025-01-04 11:46:47 -0300

    feat: replace ABOUT constant with updated content in React format

---
+++ src/content/about.const.tsx
@@ -0,0 +1,24 @@
+export const ABOUT: Array<React.ReactNode> = [
+  <>
+    Over <strong>3 years of experience</strong> as a Frontend Developer.
+  </>,
+  <>
+    Specialized in <strong>Angular</strong>, building scalable and efficient
+    single-page applications (SPAs) and microfrontend architectures.
+  </>,
+  <>
+    Bachelor’s degree in <strong>Information Systems</strong> from the{" "}
+    <strong>University of São Paulo (USP)</strong>.
+  </>,
+  <>
+    Proficient in <strong>English</strong> for professional communication.
+  </>,
+  <>
+    Enhancing my skills through self-study with <strong>React</strong> and{" "}
+    <strong>AWS</strong>.
+  </>,
+  <>
+    Dedicated to <strong>continuous learning</strong> through self-development
+    and financial literacy.
+  </>,
+];
