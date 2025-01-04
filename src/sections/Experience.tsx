b770e3f40fceb03d9b30ab8a13f3d636fa765848
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2025-01-04 17:36:50 -0300

    chore: improve layout structure in Experience section for better readability and organization

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -10,8 +10,9 @@ export default function Experience() {
       sectionNumber="03"
       nextSectionId="projects"
     >
-      <div className="flex flex-col justify-start bg-background">
+      <div className="flex flex-col bg-background">
         <div className="w-full max-w-6xl">
+          {/* Experience Header */}
           <div className="mb-8">
             <h3 className="text-xl font-semibold">
               Frontend Software Engineer{" "}
@@ -32,9 +33,11 @@ export default function Experience() {
             </p>
           </div>
         </div>
-        <div className="flex justify-start gap-8 items-center">
+
+        {/* Projects */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {EXPERIENCES.map((project) => (
-            <AppCard card={project} />
+            <AppCard card={project} key={project.cardTitle} />
           ))}
         </div>
       </div>
