317f3dd822da51a67591997e6b83cc363d9528c1
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2025-01-04 19:25:33 -0300

    chore: adjust grid layout in Experience section for improved organization

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -35,7 +35,7 @@ export default function Experience() {
         </div>
 
         {/* Projects */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
           {EXPERIENCES.map((project) => (
             <AppCard card={project} key={project.cardTitle} />
           ))}
