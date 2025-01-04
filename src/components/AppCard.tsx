c5bd8621755ae2b09fd3b029cd7cfde91a2d492f
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2025-01-04 17:36:44 -0300

    chore: adjust height constraints and text size in AppCard for better layout and readability

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -8,13 +8,14 @@ interface AppCardProps {
 
 export default function AppCard({ card }: AppCardProps) {
   return (
-    <div className="bg-background flex flex-col justify-between border border-border h-[360px] w-full rounded-lg shadow-lg p-4 transition-transform hover:scale-105">
+    <div className="bg-background flex flex-col justify-between border border-border min-h-[360px] max-h-[420px] w-full rounded-lg shadow-lg p-4 transition-transform hover:scale-105">
       {/* Card Title */}
       <div>
-        <h2 className="text-xl font-semibold text-foreground mb-1">
+        <h2 className="text-lg md:text-xl font-semibold text-foreground mb-1">
           <a
             href={card.cardTitleLink}
             target="_blank"
+            rel="noopener noreferrer"
             className="no-underline hover:text-fontColor after-line"
           >
             {card.cardTitle}
@@ -33,7 +34,6 @@ export default function AppCard({ card }: AppCardProps) {
               <span>
                 <Check className="text-fontColor mr-2 mt-1" size={12} />
               </span>
-
               {highlight}
             </li>
           ))}
