6e8a06a07727100091318044692eb8414033ea5c
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2025-01-04 19:31:51 -0300

    chore: add theme toggle component for improved user experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { AppNavigationMenu } from "./components/AppNavigationMenu";
+import { ThemeToggle } from "./components/ThemeToggle";
 import { ThemeProvider } from "./providers/theme-provider";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
@@ -11,6 +12,9 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <main className="bg-background">
+        <div className="fixed top-0 right-0 z-10 p-6">
+          <ThemeToggle />
+        </div>
         <AppNavigationMenu />
         <Welcome />
         <About />
