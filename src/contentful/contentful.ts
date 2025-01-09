29db63bf89b78f9a19e58903ce8b2cabdfc1b3af
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2025-01-08 21:27:49 -0300

    chore: add Contentful client configuration for API access

---
+++ src/contentful/contentful.ts
@@ -0,0 +1,7 @@
+import * as contentful from "contentful";
+
+export const client = contentful.createClient({
+  space: "ujdyanw7bzbf",
+  environment: "master",
+  accessToken: "CoCva5G1v57jvaMo_Wgs6tXFhouWg_vqPcw4yggzYrQ",
+});
