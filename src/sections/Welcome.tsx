983e635f39062d40f936aaf10ede9cdd32d6148e
Leonardo Grigorio Ferreira <leo.grigorio16@gmail.com>
2025-01-12 23:12:48 -0300

    refactor: simplify welcome data retrieval using useSelector

--- src/sections/Welcome.tsx
+++ src/sections/Welcome.tsx
@@ -1,40 +1,47 @@
 import AppSectionWrapper from "@/components/AppSectionWrapper";
 import { RootState } from "@/store/store";
-import { useState } from "react";
 import { useSelector } from "react-redux";
 
-interface WelcomeData {
-  introduction: string;
-  firstName: string;
-  lastName: string;
-  role: string;
-  paragraph: string;
-}
-
 export default function Welcome() {
-  const [welcomeData, setWelcomeData] = useState<null | WelcomeData>(null);
-  const { data } = useSelector((state: RootState) => {
-    console.log(state);
-    return state.content;
+  const welcomeData = useSelector((state: RootState) => {
+    if (!state.content.data) return null;
+    const _data = state.content.data.welcome;
+    return {
+      introduction: _data.introduction,
+      firstName: _data.firstName,
+      lastName: _data.lastName,
+      role: _data.role,
+      paragraph: _data.paragraph,
+    };
   });
+
+  if (!welcomeData) {
+    return (
+      <AppSectionWrapper sectionId="welcome" nextSectionId="about">
+        <div className="flex flex-col justify-center">
+          <p>Loading...</p>
+        </div>
+      </AppSectionWrapper>
+    );
+  }
 
   return (
     <AppSectionWrapper sectionId="welcome" nextSectionId="about">
       <div className="flex flex-col justify-center">
         <h1 className="text-sm md:text-md bg-gradient-to-r from-fontColor to-foreground bg-clip-text text-transparent mb-2">
-          {welcomeData?.introduction}
+          {welcomeData.introduction}
         </h1>
         <h2 className="text-4xl md:text-7xl font-bold bg-gradient-to-r from-foreground to-fontColor bg-clip-text text-transparent leading-relaxed">
-          {welcomeData?.firstName}
+          {welcomeData.firstName}
         </h2>
         <h2 className="text-4xl md:text-7xl font-bold bg-gradient-to-r from-foreground to-fontColor bg-clip-text text-transparent mb-6 !leading-tight">
-          {welcomeData?.lastName}
+          {welcomeData.lastName}
         </h2>
         <h3 className="text-2xl md:text-3xl font-semibold bg-gradient-to-r from-fontColor to-foreground bg-clip-text text-transparent mb-12">
-          {welcomeData?.role}
+          {welcomeData.role}
         </h3>
         <p className="text-sm md:text-lg font-light text-muted-foreground leading-relaxed mb-6">
-          {welcomeData?.paragraph}
+          {welcomeData.paragraph}
         </p>
       </div>
     </AppSectionWrapper>
