ae97bb8286bf00d424ac756a25dbfa16ded5e719
Leonardo Grigorio Ferreira <leo.grigorio16@gmail.com>
2025-01-13 21:39:05 -0300

    feat: fetch resume link from Redux store in AppResumeButton component

--- src/components/AppResumeButton.tsx
+++ src/components/AppResumeButton.tsx
@@ -1,12 +1,15 @@
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 import { Button } from "./ui/button";
 
 export default function AppResumeButton() {
+  const resumeLink = useSelector(
+    (state: RootState) => state.content.data?.resumeLink
+  );
+
   return (
     <Button className="hover:text-fontColor" asChild variant={"outline"}>
-      <a
-        href="https://drive.google.com/file/d/18M_mYWtrdCwetw9isrH8So5rZOAne-4u/view"
-        target="blank"
-      >
+      <a href={resumeLink} target="blank">
         Resume
       </a>
     </Button>
