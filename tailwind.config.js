c7718038c13dda4f43f2178927526d82f1b79c16
Leonardo Grigorio Ferreira <leo.grigorio16@gmail.com>
2025-02-17 20:47:06 -0300

    chore: update sans font family to Work Sans in Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      sans: ["Open Sans", "sans-serif"],
+      sans: ["Work Sans", "sans-serif"],
     },
     extend: {
       keyframes: {
